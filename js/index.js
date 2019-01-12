8f0e3e166f428b9fe85d65240acb5e5f0915643b
Juancito Peña V <38921558+JUANCITOPENA@users.noreply.github.com>
2019-01-12 17:14:01 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -56,6 +56,23 @@ function start() {
         }
     }
 
+    /*Prevent scrolling when touching the canvas*/
+    document.body.addEventListener("touchstart", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+      if (e.target == canvas) {
+        e.preventDefault();
+      }
+    }, false);
+
     function leave() {
         draw = false;
     }
